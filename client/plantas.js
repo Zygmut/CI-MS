407c08410bc541700abb75afd56159cbd9002c03
KuTo28 <toni.garri.r@gmail.com>
2024-04-15 18:59:41 +0200

    Update

--- client/plantas.js
+++ client/plantas.js
@@ -1,9 +1,25 @@
-async function call(query) {
-    let url = "./php/conexion.php?q=" + query;
+async function call(url) {
     const response = await fetch(url);
     if (!response.ok) {
         throw new Error("Error in database response: ${response.status}");
     }
 
-    return response.json();
+    return await response.json();
 }
+
+document.addEventListener("DOMContentLoaded", async () => {
+    query = await call("http://127.0.0.1:5500/client/prueba.json")
+    console.log(query.nombre)
+    console.log(query)
+    document.getElementById("plant_name").innerHTML=query.nombre
+    document.getElementById("sci_name").innerHTML="biel"
+    document.getElementById("price").innerHTML=query.coste
+    document.getElementById("plant_description").innerHTML=query.descripcion
+    for (let index = 0; index < query.caracteristicas.length; index++) {
+        document.getElementById("plant_char").innerHTML+="<li><a href=\"#!\">Web Design</a></li>"
+    }
+    
+        
+    
+});
+    
