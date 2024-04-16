2840005e3f57a1d5df200705b3b3d231e5b8574a
KuTo28 <toni.garri.r@gmail.com>
2024-04-16 08:43:45 +0200

    Added characteristics

--- client/plantas.js
+++ client/plantas.js
@@ -9,17 +9,13 @@ async function call(url) {
 
 document.addEventListener("DOMContentLoaded", async () => {
     query = await call("http://127.0.0.1:5500/client/prueba.json")
-    console.log(query.nombre)
-    console.log(query)
     document.getElementById("plant_name").innerHTML=query.nombre
     document.getElementById("sci_name").innerHTML="biel"
     document.getElementById("price").innerHTML=query.coste
     document.getElementById("plant_description").innerHTML=query.descripcion
+    console.log(query.caracteristicas[1].nombre)
     for (let index = 0; index < query.caracteristicas.length; index++) {
-        document.getElementById("plant_char").innerHTML+="<li><a href=\"#!\">Web Design</a></li>"
+        document.getElementById("plant_char").innerHTML += "<li><a style='float: right;'>" + query.caracteristicas[index].valor + "</a>" + query.caracteristicas[index].nombre + "</li>";
     }
-    
-        
-    
 });
     
